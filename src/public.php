99a71983f30ddf0b32c20a74c965fe3b3453add9
Owen Eicher <owen@tnttech.org>
2022-06-08 15:53:44 -0600

    Updated client-zone page

--- src/public.php
+++ src/public.php
@@ -5,21 +5,37 @@ $configManager = \Ubnt\UcrmPluginSdk\Service\PluginConfigManager::create();
 $config = $configManager->loadConfig();
 if (! isset($_POST["block-status"])) { 
 	$cost = floatval($api->get('surcharges/'.$config['surcharge-id'])['priceMonth1']);
-	$cost = round($cost, 2);
+	$cost = number_format((float)$cost, 2, '.', '');
 	$text='
-<body>
-<p style="color: blue;">
-TNT Tech offers network-wide advertisement blocking services for all customers. This service is completely optional, and you may change your selection at any time. It may take up to 30 minutes for changes to take effect.
-</p>
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <title>Home</title>
+    <link rel="stylesheet" href="public/index.css" media="screen">
+<link rel="stylesheet" href="Home.css" media="screen">
 
-<form action="public.php" method="post">
-        <input id="yes" type="radio" name="block-status" value="yes">
-        <label for="yes">Yes, I want to block ads</label><br>
-        <input id="no" type="radio" name="block-status" value="no">
-        <label for="no">No, I do not want to block ads</label><br>
-        <input type="submit" value="submit">
-</form>
-</body>';
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    
+  </head>
+  <body data-home-page="Home.html" data-home-page-title="Home" class="u-body u-xl-mode"><header class="u-clearfix u-header u-header" id="sec-7835"><div class="u-align-left u-clearfix u-sheet u-sheet-1"></div></header>
+    <section class="u-align-center u-clearfix u-section-1" id="sec-36ba">
+      <div class="u-align-center u-clearfix u-sheet u-sheet-1">
+        <h2 class="u-text u-text-default u-text-1">TNT Advertisement Blocking</h2>
+        <p class="u-text u-text-2"> TNT Tech offers optional network-wide advertisement blocking services for all customers which can be toggled on/off using the buttons below. Enabling this option will block the majority of internet advertisements on your entire network at a cost of $'.$cost.' per month. You may continue to see some advertisements, such as those embedded in videos<br>
+        </p>
+                <hr><br><br>
+
+                <form action="public.php" method="post">
+                        <input id="yes" type="radio" name="block-status" value="yes">
+                        <label for="yes"> Yes, I want to block ads for '.$cost.'/month</label><br>
+                        <input id="no" type="radio" name="block-status" value="no">
+                        <label for="no"> No, I do not want to block ads </label><br>
+                </form>
+      </div>
+    </section>
+  </body>
+</html>
+';
 	echo $text;
 
 } 
